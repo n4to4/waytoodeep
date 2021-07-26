13fff2ab528f8856fecbf87a3c6e7719ed15e030
n4to4 <n4to4k@gmail.com>
2021-07-26 18:25:16 +0900

    json logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use color_eyre::Report;
+use reqwest::Client;
 use tracing::info;
 use tracing_subscriber::EnvFilter;
 
@@ -7,6 +8,12 @@ async fn main() -> Result<(), Report> {
     setup()?;
 
     info!("Hello from a comfy nest we've made for ourselves");
+
+    let client = Client::new();
+    let url = "https://fasterthanli.me";
+
+    let res = client.get(url).send().await?.error_for_status()?;
+    info!(%url, content_type = ?res.headers().get("content-type"), "Got a response!");
 
     Ok(())
 }
@@ -21,6 +28,7 @@ fn setup() -> Result<(), Report> {
         std::env::set_var("RUST_LOG", "info")
     }
     tracing_subscriber::fmt::fmt()
+        .json()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
